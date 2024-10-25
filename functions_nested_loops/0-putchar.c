94644083d27a9be08eceaf8bd19ee243ffb867cd
9909-santana-herniman <9909@holbertonstudents.com>
2024-10-25 10:40:45 +0000

    Write a program that prints _putchar, followed by a new line.

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,14 +6,7 @@
  */
 int main(void)
 {
-char str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-int count, sz;
-
-sz = sizeof(str) / sizeof(int);
-for (count = 0; count < sz; count++)
-}
-_putchar(str[count]);
-}
+  system("_putchar.c");
 _putchar('\n');
 return (0);
 }
