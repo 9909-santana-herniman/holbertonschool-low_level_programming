c2faa8e7c1b7df574169eeec294485c7916ae50b
9909-santana-herniman <9909@holbertonstudents.com>
2024-10-25 09:00:00 +0000

    mandatory Write a program that prints _putchar, followed by a new line.

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
 #include <main.h>
 /*
- * main - print _putchar as an output
+ * main - print _putchar as an output using _putchar
  *
  * Return - Always 0 (Success)
  */
 int main(void)
 {
-	char output[] = "_putchar";
-	int i;
+char output[8] = {95, 112, 117, 116, 99, 104, 97, 114};
+int i;
 
-	for (i = 0; i < 26; i++)
-	{
-		_putchar(alphabet[i]);
-	}
-	_putchar('\n');
-	return (0);
+for (i = 0; i < 8; i++)
+}
+_putchar(output[i]);
+}
+_putchar('\n');
+return (0);
 }
