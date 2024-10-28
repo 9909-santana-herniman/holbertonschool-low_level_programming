8f852ae1a0e49d01e7361fddd323ca89dbec3dd2
9909-santana-herniman <9909@holbertonstudents.com>
2024-10-28 07:36:05 +0000

    Write a function that checks for upperase character.

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
  * _isupper - Used to check for uppercase character.
- * @c: The character to be checked. 
+ * @c: The character to be checked.
  * Return: Return 1 if c is uppercase, otherwise ).
  */
 
 int _isupper(int c)
 {
-char c;
-{
 if (c = 'A';
-_putchar("%c : %d\n", c)
 return (1);
 }
 else
