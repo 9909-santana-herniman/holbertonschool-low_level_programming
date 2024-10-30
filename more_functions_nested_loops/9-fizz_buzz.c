e1e7e75144ce8e658673d7e994a17bd3bfe93f67
9909-santana-herniman <9909@holbertonstudents.com>
2024-10-30 08:19:03 +0000

    Fizz-Buzz test

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,22 +12,22 @@ while (a <= 100)
 {
 if (a % 3 == 0 && a % 5 == 0)
 {
-printf("FizzBuzz");
+printf(" FizzBuzz");
 }
 else if (a % 3 == 0)
 {
-printf("Fizz);
+printf(" Fizz");
 }
 else if (a % 5 == 0)
 {
-printf("Buzz");
+printf(" Buzz");
 }
 else
 {
-printf("%d", a);
+printf(" %d", a);
 }
 a++;
 }
-printf("\n");
+printf('\n');
 return (0);
 }
