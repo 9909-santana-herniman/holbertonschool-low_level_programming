0ec4310a3b833b30e1ca251fa29c252e9d64082d
9909-santana-herniman <9909@holbertonstudents.com>
2024-10-30 08:06:25 +0000

    Fizz-Buzz test

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - FizzBuzz test
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int a = 1
+while a <= 100)
+{
+if (a % 3 == 0 && a % 5 == 0)
+{
+printf("FizzBuzz\n");
+}
+else if (a % 3 == 0)
+{
+printf("Fizz\n");
+}
+else if (a % 5 == 0)
+{
+printf("Buzz\n");
+}
+else
+{
+printf("d\n", a);
+}
+a++;
+}
+return (0);
+}
