d7cabe14128d36c76020850beb176a7bba85ab23
9909-santana-herniman <9909@holbertonstudents.com>
2024-11-01 10:36:14 +0000

    Updated main.h file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,16 +1,6 @@
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-int _isdigit(int c);
+#ifndef _main_h_
+#define _main_h_
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
