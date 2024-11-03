8c389504b1639db663c9ed1204bbaccb38c00a4d
9909-santana-herniman <9909@holbertonstudents.com>
2024-11-03 04:07:20 +0000

    Write a function that capitalises all words of a string.

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * cap_string - capitalises all words of a string.
+ * @s: string input.
+ * Return: the pointer to dest.
+ */
+char *cap_string(char *s)
+{
+int count = 0, i;
+int sow[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
+if (*(s + count) >= 97 && *(s + count) <= 122)
+*(s + count) = *(s + count) - 32;
+count++;
+while (*(s + count) != '\0')
+{
+for (i = 0; i < 13; i++)
+{
+if (*(s + count) == sow[i])
+{
+if ((*(s + (count + 1)) = *(s + (count + 1)) - 32;
+break;
+}
+}
+count++;
+}
+return (s);
+}
