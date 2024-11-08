b53db27327b5462cdcbab2dc9e6e316a41746243
9909-santana-herniman <9909@holbertonstudents.com>
2024-11-08 10:17:16 +0000

    Write a program that prints its name, followed by a new line.

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,17 +2,14 @@
 #include <stdio.h>
 /**
  * main - prints its name, followed by a new line.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
+ * @argc: argument count.
+ * @argv: argument vector.
  * Return: Always 0.
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 
 {
-if (argc > 0)
-{
-printfI"%s\n", argv[0]);
-}
+printf("%s\n", argv[0]);
 return (0);
 }
