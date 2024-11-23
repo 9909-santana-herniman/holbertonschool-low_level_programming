c558b053e4de261fa0d33ae58a6e39dda305c6fe
9909-santana-herniman <9909@holbertonstudents.com>
2024-11-23 01:11:45 +0000

    Write a function that prints anything.

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,39 +1,40 @@
 #include "variadic_functions.h"
-
 /**
  * print_all - Prints any type of argument based on the specified format.
  * @format: A list of types of arguments passed to the function.
  * Return: Nothing.
  */
-
 void print_all(const char * const format, ...)
 {
-va_list valist;
-unsigned int i = 0, c = 0;
-char *str;
-
-va_start(valist, format);
+va_list args;
+unsigned int i = 0;
+char *str, *sep = "";
+va_start(args, format);
 while (format && format[i])
 {
-if (c)
-printf(", ");
+if (format[i] == 'c' || format[i] == 'i' ||
+format[i] == 'f' || format[i] == 's')
+{
+printf("%s", sep), sep = ", ";
 switch (format[i])
 {
 case 'c':
-printf("%c", va_arg(valist, int)), c = 1;
+printf("%c", va_arg(args, int));
 break;
 case 'i':
-printf("%d", va_arg(valist, int)), c = 1;
+printf("%d", va_arg(args, int));
 break;
 case 'f':
-printf("%f", va_arg(valist, double)), c = 1;
+printf("%f", va_arg(args, double));
 break;
 case 's':
-str = va_arg(valist, char *);
-printf("%s", str ? str : "(nil)"), c = 1;
+str = va_arg(args, char *);
+printf("%s", str ? str : "(nil)");
 break;
+}
 }
 i++;
 }
-printf("\n"), va_end(valist);
+printf("\n");
+va_end(args);
 }
