916e0f41e69801c694afe4dbb14eec7a527d334c
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-17 06:38:28 +0000

    Print the string without using printf or puts

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,14 +8,7 @@
 
 int main(void)
 {
-	const char message[] =
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	size_t len = sizeof(message) - 1; /* Exclude null terminator */
-	ssize_t bytes_written = write(2, message, len);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	if (bytes_written == -1 || (size_t)bytes_written != len)
-	{
-	return(1);
-	}
-	return(1);
+	return (1);
 }
