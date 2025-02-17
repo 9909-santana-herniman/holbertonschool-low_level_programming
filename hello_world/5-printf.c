fa7b1e47eeb23577ecf0ebefe571019b048488e6
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-17 04:20:48 +0000

    Print a string using printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * printf - Use printf to display string
+ *
+ * Return: Always return 0 on successful execution.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
