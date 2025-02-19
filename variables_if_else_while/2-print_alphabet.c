6aa71406131e3003ddd44da185d03dce18cb5dfb
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-19 05:59:41 +0000

    Print the alphabet in lowercase

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	/* Declare and initailise a char array */
 	char lowercase = 'a';
-	
+
 	/* Loop while the current letter is less than or equal to z */
 	while (lowercase <= 'z')
 	{
 		putchar(lowercase); /* Print the current letter */
 		lowercase++; /* Increment the letter to move to next */
 	}
-	
+
 	/* This prints a new line */
 	putchar('\n');
 	return (0);
