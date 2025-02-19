b74bd27d08cfcd5abb86f40e4af9d6ad71f2f24f
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-19 11:44:46 +0000

    Attempting advanced task

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/* Using ASCII to determine base 10 digits */
+	int n = 48;
+	/* Loop iterates through the numbers from 48 to 57 */
+	while (n <= 57)
+	{
+		putchar(n);
+	/* Is the current number n not equal to 57 */
+	if (n != 57)
+	{
+		putchar(44); /* Insert comma */
+		putchar(32); /* Insert space */
+	}
+	n++; /* Increment number by 1 */
+	}
+
+	putchar('\n');
+	return (0);
+
+/* Adjust this code to adhere to conditions of task */
+}
