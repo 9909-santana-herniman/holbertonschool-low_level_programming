6a6c89cf68cf940d3b8636b5b9c64d1db99b8637
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-19 05:31:05 +0000

    Print the alphabet in lowercase

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -8,14 +8,18 @@
 
 int main(void)
 {
+	/* Declare and initailise a char array */
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	/* Initialise an int index to 0 */
 	int i = 0;
-
+	
+	/* Loop while the current char is NOT null terminator */
 	while (alphabet[i] != '\0')
 	{
 		putchar(alphabet[i]);
-		i++;
+		i++; /* Increment to move to next character */
 	}
+	/* This prints a new line */
 	putchar('\n');
 
 	return (0);
