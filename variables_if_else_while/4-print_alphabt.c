e62afb989e359e650f2ee4a4813303a15d55e9b3
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-19 06:52:27 +0000

    Print all letters except q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print all letters except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowercase = 'a';
+
+	while (lowercase <= 'z')
+	{
+	if (lowercase != 'q' && 'e') /* Skip the letters q and e */
+	{
+		putchar(lowercase);
+	}
+	lowercase++;
+	}
+
+	putchar('\n');
+	return (0);	
+}
