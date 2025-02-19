e2df9dc1a55de8f567f86b995c919421ad1b3e26
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-19 10:48:17 +0000

    Print single digit numbers separated by a comma, followed by a space

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,12 +8,19 @@
 
 int main(void)
 {
+	/* Using ASCII to determine base 10 digits */
 	int n = 48;
-
+	/* Loop iterates through the numbers from 48 to 57 */
 	while (n <= 57)
 	{
 		putchar(n);
-		n++;
+	/* Is the current number n not equal to 57 */
+	if (n != 57)
+	{
+		putchar(44); /* Insert comma */
+		putchar(32); /* Insert space */
+	}
+	n++; /* Increment number by 1 */
 	}
 
 	putchar('\n');
