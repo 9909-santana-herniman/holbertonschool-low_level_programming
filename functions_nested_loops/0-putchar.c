b0b15fa434dfd3297396918e477b8d19dd096c8a
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-20 03:41:36 +0000

    Create a header file, write a program to print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print _putchar, followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+
+	return (0);	
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Introduction to C Functions and nested loops
