440e96fa4ca90acae8e6784b21813ac0a5306ddc
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-21 06:08:47 +0000

    Write a function that checks for alphabetic characters

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -3,13 +3,13 @@
 /**
  * _isalpha - A function that checks for alphabetic character
  * @c: parameter is a c integer
- * Return: 1 if c is lowercase, otherwise 0.
+ * Return: 1 if c is lowercase or uppercase, otherwise 0.
  */
 
 int _isalpha(int c)
 {
 	/* Use ASCII to search range of upper and lower case letters */
-	if (c >= 97 && c <= 122)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
@@ -17,12 +17,5 @@ int _isalpha(int c)
 	{
 		return (0);
 	}
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	_putchar('\n');
 }
