5b04694499cdc546b6535fffdd15510dad6e83bb
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-22 07:02:33 +0000

    Add two integers and return the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Add two integers and return result
+ * @n1: First integer
+ * @n2: Second integer
+ * Return: Sum of two integers
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
