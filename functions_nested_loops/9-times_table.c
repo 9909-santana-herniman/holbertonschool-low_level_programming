192f7622d415a6fba92ec9d75520a49b8b4a9df9
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-23 07:52:38 +0000

    Write a function that prints the 9 times table, starting with 0

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,47 +7,40 @@
  */
 
 void times_table(void)
-{
-	int r = 0; /* r - row */
-	int c = 0; /* c - column */
-	int result; /* Stores the sum of r times c */
+{	/* r = row, c = column, result stores sum of multiplication*/
+	int r = 0, c = 0, result;
 
-	while (r <= 9) /* Loops through 0 to 9 */
+	while (r <= 9) /* Outer loop */
 	{
 		c = 0; /* Resets column to 0 at the start of each row */
 
-		while (c <= 9)
+		while (c <= 9) /* Inner loop */
 		{
-			result = r * c;
-				/* First c of the r */
-				if (c == 0)
-				{
-					_putchar(result + '0');
-				}
-				/* If result is a single digit (0-9) */
-				else if (result < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
-				}
-				/* If result is a two digit number */
-				else
-				{
-					_putchar(' ');
-					/* Prints first digit */
-					_putchar((result / 10) + '0');
-					/* Prints second digit */
-					_putchar((result % 10) + '0');
-				}
+			result = r * c; /* Multiply row by columns */
+			if (c == 0)
+			{
+				_putchar(result + '0');
+			}
+			else if (result < 10) /* Print single digit */
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else /* Print double digit */
+			{
+				_putchar(' ');
+				_putchar((result / 10) + '0'); /* Tens */
+				_putchar((result % 10) + '0'); /* Ones */
+			}
 				/* If not the last c, print ',' and ' ' */
 				if (c < 9)
 				{
 					_putchar(',');
 				}
-			c++; /* Move to next c by 1*/
+			c++;
 		}
-		_putchar('\n'); /* Print new line */
-		r++; /* Move to next r by 1 */
+		_putchar('\n');
+		r++;
 	}
 }
