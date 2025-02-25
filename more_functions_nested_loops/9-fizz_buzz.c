38e4a1973178e773f2a8223a6dfd6864e6765364
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-25 03:18:04 +0000

    The Fizz-Buzz Test

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -19,23 +19,26 @@ int main(void)
 
 	while (range <= 100)
 	{
+		/* If multiple of 3 and 5, print FizzBuzz */
 		if ((range % 3 == 0) && (range % 5 == 0))
 		{
 			printf("%s%s", num3, num5);
 		}
+		/* If multiple of 3 print Fizz */
 		else if (range % 3 == 0)
 		{
 			printf("%s", num3);
 		}
+		/* If multiple of 5 print Buzz */
 		else if (range % 5 == 0)
 		{
 			printf("%s", num5);
 		}
-		else
+		else /* If not multiple of 3 or 5, print number */
 		{
 			printf("%d", range);
 		}
-		if (range < 100)
+		if (range < 100) /* If not 100 print space */
 		{
 			putchar(' ');
 		}
