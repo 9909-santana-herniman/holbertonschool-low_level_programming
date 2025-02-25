e1366456b4bf3ce9431c5d64ae209f76518dfc32
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-25 08:33:38 +0000

    Write a function that prints a triangle, followed by a new line

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,23 +10,26 @@ void print_triangle(int size)
 {
 	int rows = 1;
 
+	/* Check if size is 0 or less, print new line */
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
+		/* Outer loop - controls the rows */
 		while (rows <= size)
 		{
 			int spaces = size - rows;
 			int hashes = rows;
 
+		/* Calculates the number of spaces to print before '#' */
 			while (spaces > 0)
 			{
 				_putchar(' ');
 				spaces--;
 			}
-
+		/* Prints '#' equal to the row number */
 			while (hashes > 0)
 			{
 				_putchar('#');
