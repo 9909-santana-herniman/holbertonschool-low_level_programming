5f0337143fd49879ab48da39bd12eb41f7d0bc32
9909-santana-herniman <santana.herniman@hotmail.com>
2025-02-26 04:24:57 +0000

    Write a function that swaps the values of two integers

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -10,6 +10,7 @@
 void swap_int(int *a, int *b)
 {
 	int c; /* Temporary int variable to hold one value during swap */
+
 	c = *a; /* Store the value pointed by a into c */
 	*a = *b; /* Replace the value pointed at a with b */
 	*b = c; /* Store the value of c (which is a) into b */
