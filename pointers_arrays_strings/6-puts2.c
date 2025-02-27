2ec14268e14f57839277b3452ec64f1ad072e28a
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-27 08:56:21 +0000

    Starting task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * puts2 - A function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ * @str: The string to be printed
+ * Return: Void
+ */
+
+void puts2(char *str)
+{
+
+}
