3348da7d853ea38ff9fc5f2a112e30092fff42a1
9909-santana-herniman <santana.herniman@gmail.com>
2025-02-27 01:45:06 +0000

    Write a function that reverses a string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,11 +8,19 @@
 
 void rev_string(char *s)
 {
+	int a;
+	int b;
+	char *start = s;
+
 	while (*s != '\0')
 	{
 		s = s + 1;
 	}
 	s = s - 1;
-	
+	while (s >= start)
+	{
+		
+	}
+	_putchar('\n');
 }
 
