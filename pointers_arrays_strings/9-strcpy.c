6f4bec1207faa7d814b36e29d9501a519f1f668b
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-01 09:05:07 +0000

    Write a function that copies the string pointed to by src, including the terminating null byte to the buffer pointed to by dest

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,18 +4,18 @@
  * _strcpy - A function that copies the string pointed to by src,
  * including the terminating null byte to the buffer pointed to by dest.
  *
- * @dest: 
- * @src:
- * Return:
+ * @dest: The pointer to the destination array where content is to be copied
+ * @src: This is the string to be copied
+ * Return: Destination buffer
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i = 0; /* Index variable */ 
 
 	while (src[i] != '\0') /* Copy characeters until null terminator */
 	{
-		dest[i] = src[i];
+		dest[i] = src[i]; /* Copy each character from src to dest */
 		i = i + 1;
 	}
 	dest[i] = '\0'; /* Ensure null terminator is copied */
