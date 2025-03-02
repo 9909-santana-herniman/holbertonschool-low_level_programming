5cfd3dd7f5ea4b395c874d1a00c79ede15ec5ac9
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-02 07:35:45 +0000

    Write a function that converts strings to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,11 +9,11 @@
 int _atoi(char *s)
 {
 	int i = 0; /* Index loop variable */
-	int sign = 1;
-	int endResult = 0;
+	int sign = 1; /* Keeps track of positive or negative */
+	int endResult = 0; /* Stores the final number*/
 	int started = 0; /* To track if we've started collecting digits */
 
-	/* Loop through characters */
+	/* Loop through characters in the string */
 	while (s[i] != '\0')
 	{
 		/* Handle the '-' */
@@ -22,11 +22,13 @@ int _atoi(char *s)
 			if (!started) /* Only adjust sign before number starts */
 			sign = sign * -1; /* Flip sign for every '-' */
 		}
-		else if (s[i] >= '0' && s[i] <= '9') /* Convert digits */
+		/* If it's a digit, convert it to an integer */
+		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			started = 1;
+			started = 1; /* Started collecting digits */
 			endResult = endResult * 10 + (s[i] - '0');
 		}
+		/* If a non-digit after starting, stop collecting */
 		else if (started)
 		{
 			return (endResult * sign);
@@ -34,5 +36,5 @@ int _atoi(char *s)
 
 		i = i + 1; /* Move to next character */
 	}
-	return (endResult * sign); /* Apply sign and return result */
+	return (endResult * sign); /* Apply sign and return final result */
 }
