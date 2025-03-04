1f442882ca34edda55fd4b42ddef508bb1e4ca21
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-04 00:48:48 +0000

    Write a function that concatenates two strings, with n as the amount of characters to copy

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,23 +11,25 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len;
-	int src_index;
+	int dest_len; /* Set length count for dest */ 
+	int src_index; /* Set index count for src */
 
 	dest_len = 0;
+	/* Find the length of dest by iterating to '\0' */
 	while (dest[dest_len] != '\0')
 	{
 		dest_len = dest_len + 1;
 	}
 
 	src_index = 0;
+	/* Append up to n characters from src to dest */
 	while (src_index < n && src[src_index] != '\0')
 	{
-		dest[dest_len] = src[src_index];
-		dest_len = dest_len + 1;
-		src_index = src_index + 1;
+		dest[dest_len] = src[src_index]; /* Copy character from src */
+		dest_len = dest_len + 1; /* Move to next position in dest */
+		src_index = src_index + 1; /* Move to next character in src */
 	}
-	dest[dest_len] = '\0';
+	dest[dest_len] = '\0'; /* Add '\0' to resutling string dest */
 
 	return (dest);
 }
