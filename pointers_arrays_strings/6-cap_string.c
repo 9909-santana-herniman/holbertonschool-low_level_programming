e8541c3d7769c51806d0c794dbad6080844bcb6a
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-05 01:44:09 +0000

    Write a function that capitalises the first letter of each word in a string

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,31 +9,40 @@
 
 char *cap_string(char *s)
 {
-	int i = 0;
-	char separators[] = " \t\n,.!?\"(){}";
-	int sep_count;
+	int i = 0; /* Start at the first character of the string */
+	char separators[] = " \t\n,.!?\"(){}"; /* List of separators */
+	int sep_count; /* Used to check each separator */
 
+	/* Loop through each character in the string until we reach null */
 	while (s[i] != '\0')
 	{
+		/* Reset to check from first separator */
 		sep_count = 0;
 
+	/* If at the start of string, and letter is lowercase, */
+	/* convert to uppercase */
 		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 		{
+		/* Convert char to uppercase by minus 32 from ASCII value */
 			s[i] = s[i] - 32;
 		}
 
+		/* Loop through each separator in the list */
 		while (separators[sep_count] != '\0')
 		{
+	/* If previous char is separator and current char is lowercase */
 			if (i > 0 && s[i - 1] == separators[sep_count])
 			{
+				/* Capitalise the current letter */
 				if (s[i] >= 'a' && s[i] <= 'z')
 				{
+					/* Convert to uppercase */
 					s[i] = s[i] - 32;
 				}
 			}
-		sep_count++;
+		sep_count++; /* Move to next separator */
 		}
-	i++;
+	i++; /* Move to the net character in the string */
 	}
-	return (s);
+	return (s); 
 }
