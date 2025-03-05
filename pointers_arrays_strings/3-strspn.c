29cbd587889de4e021389c5e08959f75a3579ceb
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-05 06:27:23 +0000

    Working on task 3. strspn

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strspn - A function that counts how many characters
+ * from the start of a string, match any character from another string.
+ * @s: Pointer to the string checked.
+ * @accept: Pointer to the string containing acceptable characters.
+ * Return: The number os characters in 's' that match any character
+ * from 'accept', starting from the beginning of 's'.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
