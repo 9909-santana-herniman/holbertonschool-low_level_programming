499dc7e6234c05e1d304f762d521cf9287ae2cb0
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-05 01:16:12 +0000

    Write a function that encodes a string in 1337 (leet speak)

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,33 +9,22 @@
 char *leet(char *s)
 {
 	int i = 0;
-	char a[] = {'a', 'A'};
-	char e[] = {'e', 'E'};
-	char o[] = {'o', 'O'};
-	char t[] = {'t', 'T'};
-	char l[] = {'l', 'L'};
+	int replace;
+	char letters[] = "aAeEoOtTlL";
+	char replacements[] = "4433007711";
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == a[0] || s[i] == a[1])
+		replace = 0;
+
+		while (letters[replace] != '\0')
 		{
-			s[i] = '4';
-		}
-		else if (s[i] == e[0] || s[i] == e[1])
-		{
-			s[i] = '3';
-		}
-		else if (s[i] == o[0] || s[i] == o[1])
-		{
-			s[i] = '0';
-		}
-		else if (s[i] == t[0] || s[i] == t[1])
-		{
-			s[i] = '7';
-		}
-		else if (s[i] == l[0] || s[i] == l[1])
-		{
-			s[i] = '1';
+			if (s[i] == letters[replace])
+			{
+				s[i] = replacements[replace];
+				break;
+			}
+		replace++;
 		}
 	i++;
 	}
