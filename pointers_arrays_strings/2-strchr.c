1e79efeb5fcbc884feb97323fcccc8291aadc805
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-05 05:20:39 +0000

    Working on task 2. strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strchr - A function that locates a character in a string.
+ * @s: Pointer to a character string.
+ * @c: Char variable to be searched.
+ * Return: First occurence of character c, otherwise NULL
+ */
+
+char *_strchar(char *s, char *c)
+{
+
+}
