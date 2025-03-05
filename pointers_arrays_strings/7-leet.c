975350d11a1a6205e713c199a8920c7f2982483f
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-05 01:28:51 +0000

    Write a function that encodes a string in 1337 (leet speak)

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,25 +8,29 @@
 
 char *leet(char *s)
 {
-	int i = 0;
-	int replace;
-	char letters[] = "aAeEoOtTlL";
-	char replacements[] = "4433007711";
+	int i = 0; /* Start at the first character of the string */
+	int rep; /* Check each letter to replace */
+	char letters[] = "aAeEoOtTlL"; /* List of letters that to convert */
+	char replace[] = "4433007711"; /* List of numbers to replace letters*/
 
+	/* Loop through each character in the string until we reach null */
 	while (s[i] != '\0')
 	{
-		replace = 0;
+		rep = 0; /* Rest rep to check the letters each time */
 
-		while (letters[replace] != '\0')
+		/* Check if current character matches any letter to replace */
+		while (letters[rep] != '\0')
 		{
-			if (s[i] == letters[replace])
+			/* If character matcher on to replace */
+			if (s[i] == letters[rep])
 			{
-				s[i] = replacements[replace];
-				break;
+				/* Replace character with number */
+				s[i] = replace[rep];
+				break; /* Once a match found, stop checking */
 			}
-		replace++;
+		rep++; /* Check next letter in the letters[] array */
 		}
-	i++;
+	i++; /* Move to next character in the string */
 	}
 	return (s);
 }
