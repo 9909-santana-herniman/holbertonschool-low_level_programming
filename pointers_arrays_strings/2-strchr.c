0fe5e4068a5dd3378cd5e70cd0d1ee591225d9c8
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-05 05:44:47 +0000

    Write a function that locates a character in a string

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,12 +2,28 @@
 
 /**
  * _strchr - A function that locates a character in a string.
- * @s: Pointer to a character string.
- * @c: Char variable to be searched.
+ * @s: Pointer to a string to search.
+ * @c: Char variable to find in string.
  * Return: First occurence of character c, otherwise NULL
  */
 
-char *_strchar(char *s, char *c)
+char *_strchr(char *s, char c)
 {
+	/* Loop through string until null terminator reached */
+	while (*s)
+	{
+		if (*s == c) /* Check if the current character matchers 'c' */
+		{
+			return (s); /* Return first occurrence */
+		}
+		s++; /* Move to next character */
+	}
 
+	/* If 'c' is null terminator, return a pointer to it */
+	if (c == '\0')
+	{
+		return (s);
+	}
+
+	return ('\0'); /* If 'c' is not found, return NULL */
 }
