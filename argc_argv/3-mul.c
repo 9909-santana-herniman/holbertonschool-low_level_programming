8d2d1d123064746cead21b26b9eea63c96e44aa4
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-11 00:53:05 +0000

    Write a program that multiplies two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that multiplies two numbers
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	/* Check number of arguments is not equal to 3 */
+	/* (program name + 2 numbers */
+	if (argc != 3)
+	{
+		printf("Error\n"); /* If incorrect, print error */
+		return (1);
+	}
+
+	/* Convert strings to integers using atoi function */
+	/* Multiply and print result */
+	printf("%d\n",atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
