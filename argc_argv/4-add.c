16318c55e1f76ecbb0443a7dd849e82a1efdae7e
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-13 01:23:58 +0000

    Update 4-add.c in argc argv

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,30 +12,35 @@
 
 int main(int argc, char *argv[])
 {
-	int sum = 0;
-	int i;
-	int j;
+	int sum = 0; /* Store sum of numbers */
+	int i; /* Loop index for iterating through arguments */
+	int j; /* Loop index for interating through char's of each argument */
 
+	/* If no numbers are provided, print "0" and exit */
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
 
+	/* Loop through each argument */
 	for (i = 1; i < argc; i++)
 	{
+		/* Loop through each character of the argument */
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
+		/* Check if each character is NOT a digit */
 			if (!isdigit(argv[i][j]))
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
+	/* Convert argument string to integer and add to sum */
 	sum = sum + atoi(argv[i]);
 
 	}
-
+	/* Print the final sum of positive numbers */
 	printf("%d\n", sum);
 	return (0);
 }
