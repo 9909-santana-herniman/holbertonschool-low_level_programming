816d63ff47d35eba07dbadbdfda4a9542a29d98a
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-18 04:03:17 +0000

    Write a program that performs simple operations

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,6 +11,7 @@ int main(int argc, char *argv[])
 {
 	int num1;
 	int num2;
+	int (*operator)(int, int);
 
 	if (argc != 4)
 	{
@@ -25,4 +26,15 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+
+	if (operator == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	operator = get_op_func(argv[2]);
+
+	printf("%d\n", operator(num1, num2);
+	return (0);
 }
