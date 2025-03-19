8f5cd0edc5a01d4b6a6a316ba659e274bdbdc323
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-19 04:14:04 +0000

    Write a function that prints anything (within reason)

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -11,39 +11,39 @@ void print_all(const char * const format, ...)
 {
 	va_list anything; /* Store variadic arguments */
 	int i = 0; /* Index for iterating through the format string*/
-	int printed = 0; /* Flag to track if a value has been printed */
 	char *string_value; /* Pointer for string arguments */
+	char *separator = "";
 
 	va_start(anything, format); /* Initialise the argument list */
 
 	while (format && format[i]) /* Loop through format string */
 	{
-		if (printed) /* Print separator before next value */
-			printf(", ");
+		/* Check if the current character is a valid type */
+		if (format[i] == 'c' || format[i] == 'i' ||
+				format[i] == 'f' || format[i] == 's')
+		{
+			printf("%s", separator);
+			separator = ", "; /* Update to a comma and space */
 
 		switch (format[i]) /* Process each format specifier */
 		{
 			case 'c': /* Print a character */
-			/* char promoted to int in variadic arguments */
 				printf("%c", va_arg(anything, int));
-				printed = 1;
 				break;
 			case 'i': /* Print an integer */
 				printf("%d", va_arg(anything, int));
-				printed = 1;
 				break;
 			case 'f': /* Print a float */
-			/* float promoted to double in variadic arguments */
 				printf("%f", va_arg(anything, double));
-				printed = 1;
 				break;
 			case 's': /* Print a string */
 				string_value = va_arg(anything, char *);
-				if (string_value == NULL)
-					string_value = "(nil)";
-				printf("%s", string_value);
-				printed = 1;
+				if (string_value != NULL)
+					printf("%s", string_value);
+				else
+					printf("%s", "(nil)");
 				break;
+		}
 		}
 	i++;
 	}
