332f7d24100fe0f7da2392254a16de837428f402
9909-santana-herniman <santana.herniman@gmail.com>
2025-03-19 04:30:16 +0000

    Update 3-main.c with comments

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,30 +11,38 @@ int main(int argc, char *argv[])
 {
 	int num1;
 	int num2;
+	/* Function pointer to hold the correct operation fucntion */
 	int (*operator)(int, int);
 
+	/* Check if number of arguments is correct */
+	/* (should be 4: program name + 3 inputs) */
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+	/* Ensure operator is a single character */
 	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
+	/* Get correct funciton for the operator */
 	operator = get_op_func(argv[2]);
 
+	/* If no match, exit with an error */
 	if (operator == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
+	/* Convert the number arguments from strings to integers */
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
+	/* Perform the operation and print the result */
 	printf("%d\n", operator(num1, num2));
 	return (0);
 }
