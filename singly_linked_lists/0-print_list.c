4fb9ef4a44770972c331f2eaa131d104e9244a17
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-01 23:45:43 +0000

    Start task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - A function that prints all the elements of a list_t list.
+ * @h: Pointer to struct node.
+ * Return: 'n' number of nodes.
+ */
+
+size_t print_list(cost list_t *h)
+{
+
+}
