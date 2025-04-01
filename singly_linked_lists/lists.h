33ec7f5c5702f10c7a2bdf6aafbb6819848fc1c1
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-01 03:56:57 +0000

    Created directory and files for linked lists

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,38 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/* Data Structure */
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+/* Function Prototypes */
+
+/* Prints all the elements */
+size_t print_list(const list_t *h);
+
+/* Returns a number of elements linked */
+size_t list_len(const list_t *h);
+
+/* Adds a new node at the beginning of a list_t list*/
+list_t *add_node(list_t **head, const char *str);
+
+/* Adds a new node at the end of a list_t list */
+list_t *add_node_end(list_t **head, const char *str);
+
+/* Frees a list_t list */
+void free_list(list_t *head);
+
+#endif
