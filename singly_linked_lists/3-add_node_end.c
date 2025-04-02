88c1fabe2833176b1fc70c5b4197d7a068724fe4
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-02 22:58:42 +0000

    Starting task 3. Add node at the end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -10,5 +10,19 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	/* Initialise a node */
+	/* Allocate memory for the node */
 
-}
+	/* Check if malloc fails */
+	/* Duplicate string to avoid modifying the original */
+	/* Free memory allocated */
+
+	/* Set length of the string */
+	/* Set the node to the previous head */
+	
+
+	/* Need new temp node to equal head*/
+	/* While temp->next is not equal to null */
+	/* temp = temp->next */
+	/* temp->next = newnode */
+
