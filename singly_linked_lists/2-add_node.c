0c1930453393cb236c75bfe1ef9c2a9e1012d313
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-02 05:43:31 +0000

    Write a function that adds a new node at the beginning of a list_t list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 #include <string.h>
 
+
+/**
+ * _strlen - Calulates the length of a string manually.
+ * @s: The string whose length is to be counted.
+ * Return: The length of the string.
+ */
+size_t _strlen(const char *s)
+{
+	size_t len = 0;
+
+	while (s[len] != '\0') /* Loop until we reach '\0' */
+	{
+		len++;
+	}
+	return (len);
+}
+
 /**
  * add_node - A function that adds a new node at the beginning
  * of a list_t list.
@@ -10,9 +27,6 @@
  * Return: The address of the new node, otherwise NULL
  * if it failed.
  */
-
-size_t _strlen(const char *s);
-
 list_t *add_node(list_t **head, const char *str)
 {
 	/* Initialised a node called 'name_node' */
@@ -43,19 +57,3 @@ list_t *add_node(list_t **head, const char *str)
 	return (name_node);
 }
 
-/**
- * _strlen - Calculates the length of a string manually.
- * @s: The string whose lenght is to be counted.
- * Return: The length of the string.
- */
-
-size_t _strlen(const char *s)
-{
-	size_t len = 0;
-
-	while (s[len] != '\0') /* Loop until we reach '\0' */
-	{
-		len++;
-	}
-	return (len);
-}
