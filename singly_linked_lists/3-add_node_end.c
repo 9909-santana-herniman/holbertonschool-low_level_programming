fb0dcbf2ea26b1ed93919114ad9a9f79df10c65b
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-02 22:40:34 +0000

    Starting task 3. Add node at the end

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * add_node_end - A function that adds a new node at the end
+ * of a list_t list.
+ * @head: Pointer to the head of the list.
+ * @str: String to be duplicated and stored in the new node.
+ * Return: The address of the new node, otherwise NULL.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+}
