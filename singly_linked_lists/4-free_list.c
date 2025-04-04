f74a1154d19759b380bcfc4924cd28e30a986a04
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-04 05:55:08 +0000

    Write a function that frees a list_t list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -7,5 +7,15 @@
 
 void free_list(list_t *head)
 {
+	/* Initialise two pointers to nodes */
+	list_t *current, *temp;
 
+	current = head; /* Start with the head of the linked list */
+
+	while (current != NULL) /* Loop until all nodes are freed */
+	{
+		temp = current; /* Store reference to current node */
+		current = current->next; /* Move to next node in the list */
+		free(temp); /* Free memory allocated for current node */
+	}
 }
