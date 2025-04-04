389a8ee4e5abeb14a0f812f28e8af073b6d0dc45
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-04 04:55:10 +0000

    Write a function that adds a new node at the end of a list_t list

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,22 @@
 #include "lists.h"
 
 /**
+ * _strlen - Calulates the length of a string manually.
+ * @s: The string whose length is to be counted.
+ * Return: The length of the string.
+ */
+size_t _strlen(const char *s)
+{
+	size_t len = 0;
+
+	while (s[len] != '\0') /* Loop until we reach '\0' */
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
  * add_node_end - A function that adds a new node at the end
  * of a list_t list.
  * @head: Pointer to the head of the list.
@@ -11,33 +27,37 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	/* Initialise a node */
-	list_t *node, *temp;
+	list_t *new_node, *temp_node;
 	/* Allocate memory for the node */
-	node = (list_t *)malloc(sizeof(list_t));
-	/* Check if malloc fails */
-	if (node == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL) /* Check if malloc fails */
 	{
 		return (NULL);
 	}
 	/* Duplicate string to avoid modifying the original */
-	node->str = strdup(str);
+	new_node->str = strdup(str);
 	/* Free memory allocated */
-	if (node->str == NULL)
+	if (new_node->str == NULL)
 	{
-		free(node);
+		free(new_node);
 		return (NULL);
 	}
 	/* Set length of the string */
-	node->len = _strlen(str);
+	new_node->len = _strlen(str);
 	/* Set the node to the previous head */
-	node->next = NULL;
+	new_node->next = NULL;
 	/* Need new temp node to equal head*/
-	temp = *head;
-	/* While temp->next is not equal to null */
-	while (temp->next != NULL)
+	if (*head == NULL) /* Check if the linked list is empty */
 	{
-		temp = temp->next;
+		*head = new_node; /* Head points to new node */
+		return (new_node);
 	}
-	temp->next = node;
-	return (node);
+	temp_node = *head; /* Otherwise, traverse to the end of the list */
+	while (temp_node->next != NULL) /* Move through each node */
+	{
+		temp_node = temp_node->next; /* Go to next node */
+	}
+
+	temp_node->next = new_node; /* Link the last node to the new node */
+	return (new_node); /* Return address of the new node */
 }
