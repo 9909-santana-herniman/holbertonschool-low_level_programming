63a9ce3de4691af36d75a8b0f17968e4b1b8ed61
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-04 04:36:03 +0000

    Fixing compiling issue

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -2,6 +2,22 @@
 #include <string.h>
 
 /**
+ * _strlen - Calulates the length of a string manually.
+ * @s: The string whose length is to be counted.
+ * Return: The length of the string.
+ */
+size_t _strlen(const char *s)
+{
+	size_t len = 0;
+
+	while (s[len] != '\0') /* Loop until we reach null terminator */
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
  * add_node - A function that adds a new node at the beginning
  * of a list_t list.
  * @head: Pointer to the head of the list.
@@ -12,31 +28,30 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	/* Initialised a node called 'name_node' */
-	list_t *name_node;
-	/* Allocate memory for name_node */
-	name_node = (list_t *)malloc(sizeof(list_t));
+	/* Initialised a node */
+	list_t *new_node;
+	/* Allocate memory for new_node */
+	new_node = (list_t *)malloc(sizeof(list_t));
 
-	if (name_node == NULL)
+	if (new_node == NULL)
 	{
 		return (NULL); /* If malloc fails, return NULL */
 	}
 
 	/* Duplicate string to avoid modifying the original */
-	name_node->str = strdup(str);
+	new_node->str = strdup(str);
 
-	if (name_node->str == NULL)
+	if (new_node->str == NULL)
 	{
-		free(name_node); /* Free memory allocated for the node */
+		free(new_node); /* Free memory allocated for the node */
 		return (NULL);
 	}
 	/* Set the length of the string */
-	name_node->len = _strlen(str);
+	new_node->len = _strlen(str);
 	/* Set the name_node's 'next' to the previous head */
-	name_node->next = *head;
+	new_node->next = *head;
 	/* Change 'head' to point to recently created node */
-	*head = name_node;
+	*head = new_node;
 
-	return (name_node);
+	return (new_node);
 }
-
