dde67832fc4d0a9108c2e2c332cb98a4d86472d5
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-09 02:12:51 +0000

    Write a program that copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,65 +2,81 @@
 #include <stdio.h>
 
 /**
- * error_exit - Prints an error message to stderr and exits with a given code.
- * @code: The exit code.
- * @message: The error message format string.
- * @arg: The argument to include in the error message.
+ * error_file - Handles errors related to opening files.
+ * @file_from: File descriptor for the source file.
+ * @file_to: File descriptor for the destination file.
+ * @argv: Arguments vector (contains filenames).
  */
 
-void error_exit(int code, const char *message, const char *arg)
+void error_file(int file_from, int file_to, char *argv[])
 {
-	dprintf(STDERR_FILENO, message, arg);
-	exit(code);
+	if (file_from == -1) /* Error opening source file */
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1) /* Error opening destination file */
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
 
 /**
- * main - Copies the content of a file to another file.
- * @argc: The number of arguments.
- * @argv: The array of arguments.
- * Return: 0 on success, otherwise exit with error code on failure.
+ * main - Copies the content of one file to another.
+ * @argc: Number of arguments passed to the program.
+ * @argv: Array of arguments (contains filenames).
+ * Return: Always 0 on success, otherwise exits with specific code.
  */
 
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to; /* FD for source and destination files */
-	ssize_t bytes_read, bytes_written; /* Bytes read and written */
-	char buffer[1024]; /* Buffer for reading and writing */
+	int fd_from, fd_to;
+	int error_close;
+	ssize_t bytes_read, bytes_written;
+	char buffer[1024];
 
-	/* Check the number of arguments */
+	/* Check if the correct number of arguments is provided */
 	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to\n", NULL);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-	/* Open the source file (file_from) */
+	/* Open the source file in read-only mode */
 	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
 
-	/* Open or create the destination file (file_to) */
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	/* Open or create the destination file in write mode */
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	/* Read from source and write to destination in chunks */
+	/* Handle errors related to opening files */
+	error_file(fd_from, fd_to, argv);
+
+	/* Copy data from src to dest in chunks of up to 1024 bytes */
 	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1 || bytes_written != bytes_read)
-			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+			error_file(0, -1, argv);
 	}
+	
+	/* Handle errors related to reading from src file */
 	if (bytes_read == -1)
-	{
-		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-	}
+		error_file(-1, 0, argv);
 
-	/* Close both files */
-	if (close(fd_from) == -1)
+	/* Close both files and handle errors during closing */
+	error_close = close(fd_from);
+	if (error_close == -1)
 	{
-		error_exit(100, "Error: Can't close fd %d\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
 	}
-	if (close(fd_to) == -1)
+	error_close = close(fd_to);
+	if (error_close == -1)
 	{
-		error_exit(100, "Error: Can't close fd %d\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
 	}
 	return (0);
+
 }
