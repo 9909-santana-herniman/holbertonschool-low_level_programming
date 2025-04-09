470c02d9288862990e130a20a96b6933e6493983
9909-santana-herniman <santana.herniman@gmail.com>
2025-04-09 01:15:48 +0000

    Write a program that copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -49,7 +49,10 @@ int main(int argc, char *argv[])
 			error_exit(99, "Error: Can't write to %s\n", argv[2]);
 	}
 	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "DEBUG: Read failed for file %s\n", argv[1]);
 		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	}
 
 	/* Close both files */
 	if (close(fd_from) == -1)
